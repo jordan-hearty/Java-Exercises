91c133f424fde099a10b7d82e213a7852d40a634
Jordan Hearty <jordan16hearty@gmail.com>
2018-03-20 18:06:03 +0000

    Add files via upload
    My first project using Netbeans IDE. Using integers to print result

---
+++ FirstProject/src/firstproject/FirstProject.java
@@ -0,0 +1,34 @@
+/*
+ My first Java Program
+ */
+package firstproject;
+
+import java.time.Clock;
+
+/**
+ *
+ * @author Jordan
+ */
+public class FirstProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // First statement
+        System.out.println("Hello World");
+        
+        int x = 10;
+        int y = 3;
+        int result = x % y;
+        System.out.println("result = " + result);
+        
+        //& = MODULUS or Concatanator
+        /* 3 goes into 10 3 TIMES, remainder 1
+           So the number "1" is the result
+        
+        */
+        
+    }
+    
+}
